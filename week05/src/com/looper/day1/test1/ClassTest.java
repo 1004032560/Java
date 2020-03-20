5c4acbf3a204a7b9117389c694beedf9be4b716e
1004032560 <1004032560@qq.com>
2020-03-20 17:45:11 +0800

    反射-三种获取Class类实例的方式

--- week05/src/com/looper/day1/test1/ClassTest.java
+++ week05/src/com/looper/day1/test1/ClassTest.java
@@ -2,23 +2,20 @@ package com.looper.day1.test1;
 
 public class ClassTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 
         //方式一：
-        Class Class1 = Student.class;
-        System.out.println(Class1);
+        Class class1 = Student.class;
+        System.out.println(class1);
 
         //方式二：
         Student student = new Student();
-        Class Class2 = student.getClass();
-        System.out.println(Class2);
+        Class class2 = student.getClass();
+        System.out.println(class2);
 
         //方式三：
-        try {
-            Class Class3 = Class.forName("com.looper.day1.test1.Student.");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        Class class3 = Class.forName("com.looper.day1.test1.Student");
+        System.out.println(class3);
 
     }
 }
--- week05/src/com/looper/day1/test1/Student.java
+++ week05/src/com/looper/day1/test1/Student.java
@@ -5,4 +5,36 @@ public class Student {
     private int id;
     private String name;
 
+    //在反射中，空构造必须写
+    public Student() {
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
