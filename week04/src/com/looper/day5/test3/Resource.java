edc42c8d355f14092976b94e78364c12bc26096d
1004032560 <1004032560@qq.com>
2020-03-20 11:15:32 +0800

    多线程-练习-多线程同步,synchronized同步方法:理解synchronized的锁对象是this

--- week04/src/com/looper/day5/test3/Resource.java
+++ week04/src/com/looper/day5/test3/Resource.java
@@ -3,23 +3,27 @@ package com.looper.day5.test3;
 public class Resource {
 
     public synchronized void f1(){
-        for (int i=0;i<10;i++){
-            System.out.println(Thread.currentThread().getName()+":i="+i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        //synchronized (new Object()){
+            for (int i=0;i<10;i++){
+                System.out.println(Thread.currentThread().getName()+":i="+i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
+        //}
     }
 
     public synchronized void m1(){
-        for (int i=0;i<10;i++){
-            System.out.println(Thread.currentThread().getName()+":i="+i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        synchronized (new Object()) {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + ":i=" + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -29,19 +33,14 @@ public class Resource {
         //synchronized方法锁的是一个对象，只有f1执行完了才会执行m1
         Resource resource = new Resource();
 
-        Thread thread1 = new Thread(){
-            @Override
-            public void run() {
+        Thread thread1 = new Thread(() -> {
                 resource.f1();
-            }
-        };
+        });
 
-        Thread thread2 = new Thread(){
-            @Override
-            public void run() {
+        Thread thread2 = new Thread(() -> {
                 resource.m1();
-            }
-        };
+
+        });
 
         thread1.start();
         thread2.start();
