ade88f6ac18104f834bd9ab058bb3a3d1706e585
1004032560 <1004032560@qq.com>
2020-03-20 18:27:22 +0800

    反射-通过反射创建对对象

---
+++ week05/src/com/looper/day1/test2/ClassTest.java
@@ -0,0 +1,24 @@
+package com.looper.day1.test2;
+
+import java.lang.reflect.Method;
+
+public class ClassTest {
+
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+
+        Class class1 = Student.class;
+        System.out.println(class1);
+
+        //使用反射动态创建对象
+        //向下造型
+        Student student = (Student) class1.newInstance();
+        System.out.println("通过反射创建出的对象："+student);
+
+        Method [] methods = class1.getMethods();
+        for (int i = 0; i < methods.length; i++) {
+            System.out.println(methods[i]);
+        }
+
+    }
+
+}
