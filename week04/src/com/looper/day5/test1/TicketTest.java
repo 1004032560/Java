bb172f301aab145c382e751e98789c74c7540689
1004032560 <1004032560@qq.com>
2020-03-20 10:15:19 +0800

    多线程-练习-多线程同步:实现窗口售票,产生数据问题

--- week04/src/com/looper/day5/test1/TicketTest.java
+++ week04/src/com/looper/day5/test1/TicketTest.java
@@ -3,8 +3,9 @@ package com.looper.day5.test1;
 public class TicketTest {
 
     public static void main(String[] args) {
-        Thread thread1 = new Thread(new Ticket(),"窗口1");
-        Thread thread2 = new Thread(new Ticket(),"窗口2");
+        Ticket ticket = new Ticket();
+        Thread thread1 = new Thread(ticket,"窗口1");
+        Thread thread2 = new Thread(ticket,"窗口2");
 
         thread1.start();
         thread2.start();
