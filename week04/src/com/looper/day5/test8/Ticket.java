7e0933e3208a0607b0a392bd2c374a26599788cd
1004032560 <1004032560@qq.com>
2020-03-20 16:05:52 +0800

    多线程-新工具-Lock替换synchronized

---
+++ week04/src/com/looper/day5/test8/Ticket.java
@@ -0,0 +1,35 @@
+package com.looper.day5.test8;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Ticket implements Runnable {
+
+    private int count = 100;
+
+    static Object object = new Object();
+
+    Lock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+
+        while (true) {
+            lock.lock();
+            if (count >= 1) {
+                System.out.println(Thread.currentThread().getName() + "：出售第" + count + "张票");
+                count--;
+                //放慢速度，模拟真实环境
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("票已经被出售完了");
+                break;
+            }
+            lock.unlock();
+        }
+    }
+}
---
+++ week04/src/com/looper/day5/test8/TicketTest.java
@@ -0,0 +1,14 @@
+package com.looper.day5.test8;
+
+public class TicketTest {
+
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket();
+        Thread thread1 = new Thread(ticket,"窗口1");
+        Thread thread2 = new Thread(ticket,"窗口2");
+
+        thread1.start();
+        thread2.start();
+    }
+
+}
