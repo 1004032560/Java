f8ba25473fb0517d2fceee8cb3929455bee0e2a7
1004032560 <1004032560@qq.com>
2020-04-03 21:57:11 +0800

    test

--- Test/src/com/looper/day1/test2/test/Test.java
+++ Test/src/com/looper/day1/test2/test/Test.java
@@ -1,4 +1,13 @@
 package com.looper.day1.test2.test;
 
 public class Test {
+
+    public static void main(String[] args) {
+
+        int [] a = new int[3];
+
+        int b = a.length;
+
+    }
+
 }
